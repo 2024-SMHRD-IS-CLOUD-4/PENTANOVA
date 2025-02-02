161118aa73d5aee5e190c67a9f9ac2e7160fd9f1
huiju <ohj4875@gmail.com>
2025-02-02 21:18:46 +0900

    front_userJoinPage_수정

--- Front/src/App.js
+++ Front/src/App.js
@@ -4,28 +4,28 @@ import Login from './component/Login';
 import Join from './component/jip/Join';
 import IdFind from './component/jip/IdFind';
 import PwFind from './component/jip/PwFind';
+import Diagnosis from './component/user/Diagnosis';
+import AiDiagnosis from './component/user/AiDiagnosis';
+import SelfDiagnosis from './component/user/SelfDiagnosis';
+import HisDiagnosis from './component/user/HisDiagnosis';
+import DpDetail from './component/user/DpDetail';
+import DpList from './component/user/DpList';
+import MyProfile from './component/user/MyProfile';
+import FumigatorPesticides from './component/user/FumigatorPesticides';
 import Navbar from './component/NavBar';
 import Dashboard from './component/Dashboard'; // 각 페이지 컴포넌트 import
 import PestManagement from './component/PestManagement';
 import PromotionManagement from './component/PromotionManagement';
 import UserManagement from './component/UserManagement';
-import Diagnosis from './component/user/Diagnosis';
-import DpDetail from './component/user/DpDetail';
-import DpList from './component/user/DpList';
-import HisDiagnosis from './component/user/HisDiagnosis';
-import SelfDiagnosis from './component/user/SelfDiagnosis';
 import FieldGuide from './component/FieldGuide';
 import UserDetail from './component/UserDetail';
 import DiagDetail from './component/DiagDetail';
-import FumigatorPesticides from './component/user/FumigatorPesticides';
-import MyProfile from './component/user/MyProfile';
-import { AppData } from './function/AuthContext';
-import Uploader from './component/user/AiDiagnosis';
 import ChangeProfile from './component/ChangeProfile';
 import RequestAuth from './component/RequestAuth';
 import JipJoinPage from './component/JipJoinPage';
 import CropList from './component/CropList';
 import UserJoinPage from './component/UserJoinPage';
+import { AppData } from './function/AuthContext';
 
 function App() {
   const storedUser = sessionStorage.getItem("user");
@@ -43,25 +43,25 @@ function App() {
             <Route path="/pwFind" element={<PwFind />} />
             <Route path="/requestAuth" element={<RequestAuth />} />
             <Route path="/pest" element={<PestManagement />} />
-            <Route path="/uploader" element={<Uploader />} />
+            <Route path="/aiDiagnosis" element={<AiDiagnosis />} />
             <Route path="/promotion" element={<PromotionManagement />} />
             <Route path="/users" element={<UserManagement />} />
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/diagnosis" element={<Diagnosis />}></Route>
-            <Route path="/dpDetail" element={<DpDetail />}></Route>
-            <Route path="/dpList" element={<DpList />}></Route>
-            <Route path="/userDetail" element={<UserDetail />}></Route>
-            <Route path="/diagDetail" element={<DiagDetail />}></Route>
-            <Route path="/hisDiagnosis" element={<HisDiagnosis />}></Route>
-            <Route path="/selfDiagnosis" element={<SelfDiagnosis />}></Route>
-            <Route path="/fieldGuide" element={<FieldGuide />}></Route>
-            <Route path="/myProfile" element={<MyProfile />}></Route>
-            <Route path="/changeProfile" element={<ChangeProfile />}></Route>
-            <Route path="/fumigatorPesticides" element={<FumigatorPesticides />}></Route>
-            <Route path="/auth" element={<FumigatorPesticides />}></Route>
-            <Route path="/crops" element={<CropList />}></Route>
-            <Route path="/UserJoinPage" element={<UserJoinPage />}></Route>
+            <Route path="/diagnosis" element={<Diagnosis />} />
+            <Route path="/dpDetail" element={<DpDetail />} />
+            <Route path="/dpList" element={<DpList />} />
+            <Route path="/userDetail" element={<UserDetail />} />
+            <Route path="/diagDetail" element={<DiagDetail />} />
+            <Route path="/hisDiagnosis" element={<HisDiagnosis />} />
+            <Route path="/selfDiagnosis" element={<SelfDiagnosis />} />
+            <Route path="/fieldGuide" element={<FieldGuide />} />
+            <Route path="/myProfile" element={<MyProfile />} />
+            <Route path="/changeProfile" element={<ChangeProfile />} />
+            <Route path="/fumigatorPesticides" element={<FumigatorPesticides />} />
+            <Route path="/auth" element={<FumigatorPesticides />} />
+            <Route path="/crops" element={<CropList />} />
+            <Route path="/UserJoinPage" element={<UserJoinPage />} />
           </Routes>
         </AppData>
       </div>
