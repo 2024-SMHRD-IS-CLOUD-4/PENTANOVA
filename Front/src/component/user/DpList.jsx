c6b7b640647b70e028f96ba3a3de9964645e01da
yunSeong <diddbstjd9@gmail.com>
2025-02-05 17:17:22 +0900

    ys commit - dpList 수정

--- Front/src/component/user/DpList.jsx
+++ Front/src/component/user/DpList.jsx
@@ -9,19 +9,23 @@ const DpList = () => {
     const [dps, setDps] = useState([]);
     const [searchParams] = useSearchParams();
     const crop_num = searchParams.get('crop_num');
-    let [asd, setAsd] = useState([]) ;
+    let [asd, setAsd] = useState([]);
     useEffect(() => {
         const dpList = async () => {
             try {
                 const response = await axios.get(`${process.env.REACT_APP_connect}/dp/dpList`);
                 console.log(response.data);
-                response.data.map((dp, idx) => (
-                    dpData.data.map(dp_num=>{
-                        if(dp.dp_num==dp_num){
-                            setDps(dps => [...dps, dp])
-                        }
-                    })
-                ))
+                response.data.map((dp, idx) => {
+                    if (dpData.data[0]) {
+                        dpData.data.map(dp_num => {
+                            if (dp.dp_num == dp_num) {
+                                setDps(dps => [...dps, dp])
+                            }
+                        })
+                    } else {
+                        setDps(dps => [...dps, dp])
+                    }
+                })
             } catch (error) {
                 console.error('Error:', error);
             }
@@ -29,6 +33,12 @@ const DpList = () => {
         };
         dpList();
     }, []);
+    console.log(dpData.data[0]);
+    if (dpData.data[0]) {
+        console.log("qweqdas");
+    } else {
+        console.log("없음");
+    }
     return (
         <div>
             <h2>병해충 도감</h2>
