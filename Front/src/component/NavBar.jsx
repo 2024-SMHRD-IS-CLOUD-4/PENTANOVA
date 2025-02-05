15786a66ca8bd2c3b354a84a19091daa94ec7ba5
yunSeong <diddbstjd9@gmail.com>
2025-02-05 11:29:33 +0900

    ys commit

--- Front/src/component/NavBar.jsx
+++ Front/src/component/NavBar.jsx
@@ -50,68 +50,68 @@ const Navbar = () => {
       </ul>
     );
     if (shareData.data) {
-        if (shareData.data.isTrue2) {
-          setNavContent(
-            <ul>
-              <li>
-                <NavLink to="/dashboard" className={isActive => isActive ? 'active' : ''}>
-                  대시보드
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/pest" className={isActive => isActive ? 'active' : ''}>
-                  병해충 정보 관리
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/promotion" className={isActive => isActive ? 'active' : ''}>
-                  홍보문구 관리
-                </NavLink>
-              </li>
-              {shareData.data.role=='최고관리자'?<li>
-                <NavLink to="/users" className={isActive => isActive ? 'active' : ''}>
-                  사용자 관리
-                </NavLink>
-              </li>:null}
-              <li>
-                <NavLink to="/" onClick={() => logout()} className={isActive => isActive ? 'active' : ''}>
-                  로그아웃
-                </NavLink>
-              </li>
-            </ul>
-          );
-        } else {
-          setNavContent(
-            <ul>
-              <li>
-                <NavLink to="/diagnosis" className={isActive => isActive ? 'active' : ''}>
-                  병해충 진단
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/fieldGuide" className={isActive => isActive ? 'active' : ''}>
-                  병해충 도감
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/fumigatorPesticides" className={isActive => isActive ? 'active' : ''}>
-                  농약 및 방제 정보
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/myProfile" className={isActive => isActive ? 'active' : ''}>
-                  내 정보 확인하기
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/" onClick={() => logout()} className={isActive => isActive ? 'active' : ''}>
-                  로그아웃
-                </NavLink>
-              </li>
-            </ul >
-          );
-        }
+      if (shareData.data.isTrue2) {
+        setNavContent(
+          <ul>
+            <li>
+              <NavLink to="/dashboard" className={isActive => isActive ? 'active' : ''}>
+                대시보드
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/pest" className={isActive => isActive ? 'active' : ''}>
+                병해충 정보 관리
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/promotion" className={isActive => isActive ? 'active' : ''}>
+                홍보문구 관리
+              </NavLink>
+            </li>
+            {shareData.data.role == '최고관리자' ? <li>
+              <NavLink to="/users" className={isActive => isActive ? 'active' : ''}>
+                사용자 관리
+              </NavLink>
+            </li> : null}
+            <li>
+              <NavLink to="/" onClick={() => logout()} className={isActive => isActive ? 'active' : ''}>
+                로그아웃
+              </NavLink>
+            </li>
+          </ul>
+        );
+      } else {
+        setNavContent(
+          <ul>
+            <li>
+              <NavLink to="/diagnosis" className={isActive => isActive ? 'active' : ''}>
+                병해충 진단
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/fieldGuide" className={isActive => isActive ? 'active' : ''}>
+                병해충 도감
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/fumigatorPesticides" className={isActive => isActive ? 'active' : ''}>
+                농약 및 방제 정보
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/myProfile" className={isActive => isActive ? 'active' : ''}>
+                내 정보 확인하기
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" onClick={() => logout()} className={isActive => isActive ? 'active' : ''}>
+                로그아웃
+              </NavLink>
+            </li>
+          </ul >
+        );
       }
+    }
 
   }, [shareData.data, navigate]);
 
--- Front/src/component/user/SelfDiagnosis.jsx
+++ Front/src/component/user/SelfDiagnosis.jsx
@@ -5,36 +5,29 @@ import logo from '../../assets/logo.png'
 import axios from 'axios'
 
 const SelfDiagnosis = () => {
-  // const [dps, setDps] = useState([]);
-  const dps = [
-    {
-      name: "사과",
-      crop: {
-        crop_num: 1
-      }
-    },
-    {
-      name: "배",
-      crop: {
-        crop_num: 2
-      }
-    },
-    {
-      name: "감",
-      crop: {
-        crop_num: 3
-      }
-    }
-  ];
+  const [dps, setDps] = useState([]);
+  const [crops, setCrops] = useState([]);
+  const [selected, setSelected] = useState("토픽 선택");
+  const months = [1,2,3,4,5,6,7,8,9,10,11,12];
+  const sites = ['잎', '과실', '가지']
   useEffect(() => {
     const dpList = async () => {
       const response = await axios.get(`${process.env.REACT_APP_connect}/dp/dpList`);
-      // setDps(response.data);
-
+      setDps(response.data);
+      console.log(response.data)
+    }
+    const cropList = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_connect}/crop/cropList`);
+      setCrops(response.data);
+      console.log(response.data)
     }
     dpList();
-    console.log(dps)
+    cropList();
   }, [])
+
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
 
   return (
     <div id='sdMainBox'>
@@ -42,9 +35,12 @@ const SelfDiagnosis = () => {
       <ul>
         <li>
           <p>작물 선택 <span style={{ color: 'red' }}>(필수)</span></p>
-          <select name="crops">
-            {dps.map(dp => {
-              <option key={dp.crop} value="choiseNone">{dp.crop.name}</option>
+          <select name="crops" onChange={handleSelect} value={selected}>
+            <option>작물 선택</option>
+            {crops.map((crop, idx) => {
+              return <option key={idx} value={crop.crop_num}>
+                {crop.name}
+              </option>;
             })}
           </select>
         </li>
@@ -52,18 +48,27 @@ const SelfDiagnosis = () => {
           <p>발병 시기 <span>(선택)</span></p>
           <select name="crops">
             <option value="choiseNone">월 선택</option>
+            {months.map(month=>{
+              return <option value={month}>{month}월</option>
+            })}
           </select>
         </li>
         <li>
           <p>피해 위치 <span style={{ color: 'red' }} >(필수)</span></p>
-          <select name="part">
+          <select name="part" onChange={handleSelect} value={selected}>
             <option value="choiseNone">작물 피해 위치 선택</option>
+            {sites.map(site=>{
+              return <option value={site}>{site}</option>
+            })}
           </select>
         </li>
         <li>
           <p>피해 특징 <span>(선택)</span></p>
-          <select name="period" id="">
+          <select name="period" onChange={handleSelect} value={selected}>
             <option value="choiseNone">작물 피해 특징 선택</option>
+            {dps.map(dp=>{
+              return <option>{dp.argu}</option>
+            })}
           </select>
         </li>
       </ul>
